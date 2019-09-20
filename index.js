935007fce93fd06a2d8a3ae5aca211b97cff63d3
Tony Baker <this.tonybaker@gmail.com>
2019-09-20 23:14:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function writeCards(namesArray, eventName) {
-  for (let i = 0, i < namesArray.length, i++)
+  for (let i = 0; i < namesArray.length; i++)
     console.log(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
 }
 
