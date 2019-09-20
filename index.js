3bc579917205ff3a4439c8670898576aa66cd58e
Tony Baker <this.tonybaker@gmail.com>
2019-09-20 23:41:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+  let messageArray = []
+  for (let i = 0; i < namesArray.length; i++) {
+    messageArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messageArray;
+}
+
+function countdown(number) {
+  let n = number;
+  while (n >= 0) {
+    console.log(n--);
+  }
+}
